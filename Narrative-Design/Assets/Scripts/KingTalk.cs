d7b68b672dc3621463e00178b110aaef10b9d5b6
8BitPerc30 <declanfinley@gmail.com>
2024-11-06 01:28:41 -0500

    Rat king can SPEAK!

---
+++ Narrative-Design/Assets/Scripts/KingTalk.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using Yarn.Unity;
+
+public class KingTalk : MonoBehaviour
+{
+    private bool playerInRange;
+    public DialogueRunner dialogueRunner;
+    public string startNode = "RatKing";
+
+    // Reference to the player and their movement script
+    public GameObject player;
+    private PlayerMovement playerMovement;
+
+    void Start()
+    {
+        // Ensure playerMovement is assigned
+        if (player != null)
+        {
+            playerMovement = player.GetComponent<PlayerMovement>();
+        }
+    }
+
+    void Update()
+    {
+        // Check if the player is in range and has pressed the E key
+        if (playerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            // If the dialogue is not running, start the dialogue
+            if (!dialogueRunner.IsDialogueRunning)
+            {
+                dialogueRunner.StartDialogue(startNode);
+            }
+        }
+
+        // Disable player movement if the dialogue is running, otherwise enable it
+        if (playerMovement != null)
+        {
+            playerMovement.enabled = !dialogueRunner.IsDialogueRunning;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+}
