031285650d551c8398a29e71c8b4f6efbb4a012d
drawssua <200614345@student.georgianc.on.ca>
2024-11-06 00:09:13 -0500

    Castle transition and taverm

---
+++ Narrative-Design/Assets/Scripts/EnterCastle.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnterCastle : MonoBehaviour
+{
+    private bool playerInRange;
+
+    void Update()
+    {
+        if (playerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            SceneManager.LoadScene("Castle");
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+}
